008791fa4f80762ad2572cff448257bbba6d98be
Aditi Singh <abroad32197679@gmail.com>
2025-04-12 09:42:56 +0530

    Delete backend/routes/reportRoutes.js

--- backend/routes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { generateReport } = require("../controllers/reportController");
-
-router.get("/:type/:userId", generateReport); // type = daily, weekly, monthly
-
-module.exports = router;
