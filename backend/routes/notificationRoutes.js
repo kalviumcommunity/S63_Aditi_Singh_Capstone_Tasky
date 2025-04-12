81ade76f9eee007566ade0e1cd962ea400f70ca3
Aditi Singh <abroad32197679@gmail.com>
2025-04-12 09:42:13 +0530

    Delete backend/routes/notificationRoutes.js

--- backend/routes/notificationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getUserNotifications, markAsRead } = require("../controllers/notificationController");
-
-router.get("/:userId", getUserNotifications);
-router.put("/:id/read", markAsRead);
-
-module.exports = router;
-
