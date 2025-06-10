6a48aa693a6716183973a69e1e2296355db18787
Adzy11 <abroad32197679@gmail.com>
2025-06-10 22:39:08 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 const app = express();
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5173', 'https://xzc.netlify.app/'], // Replace with your actual Netlify domain
+  origin: 'https://xzc.netlify.app',
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
@@ -37,13 +37,13 @@ app.use(cookieSession({
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/api/users", userRoutes);
-app.use("/api/tasks", taskRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/reports", reportRoutes);
-app.use("/api/notifications", notificationRoutes);
-app.use("/api/events", eventRoutes);
-app.use("/api/team-members", teamMemberRoutes);
+app.use("/users", userRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/admin", adminRoutes);
+app.use("/reports", reportRoutes);
+app.use("/notifications", notificationRoutes);
+app.use("/events", eventRoutes);
+app.use("/team-members", teamMemberRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
